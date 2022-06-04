191a67417ab77c34b946e5d40433e8ccfb987c69
imtiazmlbd <imtiaz.uddin@monstar-lab.com>
2022-06-04 16:21:53 +0600

    feat(viewmodel): StartViewModel added

---
+++ app/src/main/java/com/example/androidsimpleboilerplates/presentation/start/StartViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.androidsimpleboilerplates.presentation.start
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.androidsimpleboilerplates.core.extensions.Resource
+import com.example.androidsimpleboilerplates.domain.model.PicInfo
+import com.example.androidsimpleboilerplates.domain.usecase.GetPicInfoUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class StartViewModel @Inject constructor(
+    private val getPicInfoUseCase: GetPicInfoUseCase
+) : ViewModel() {
+
+    var picInfoFlow: Flow<Resource<PicInfo>> = MutableStateFlow<Resource<PicInfo>>(Resource.Empty())
+
+    init {
+        viewModelScope.launch {
+            picInfoFlow = getPicInfoUseCase.execute()
+        }
+    }
+
+}
