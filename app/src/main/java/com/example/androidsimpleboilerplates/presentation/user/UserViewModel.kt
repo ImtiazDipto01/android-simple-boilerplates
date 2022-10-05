457d7216dff1a30c7c98dfe70e1470dacf2ca2f8
imtiazmlbd <imtiaz.uddin@monstar-lab.com>
2022-10-05 16:18:16 +0600

    feat: recyclerview added into fragment_users. and recyclerview item(item_user) ui added

--- app/src/main/java/com/example/androidsimpleboilerplates/presentation/user/UserViewModel.kt
+++ app/src/main/java/com/example/androidsimpleboilerplates/presentation/user/UserViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.androidsimpleboilerplates.presentation.start
+package com.example.androidsimpleboilerplates.presentation.user
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
--- app/src/main/java/com/example/androidsimpleboilerplates/presentation/user/UsersFragment.kt
+++ app/src/main/java/com/example/androidsimpleboilerplates/presentation/user/UsersFragment.kt
@@ -1,4 +1,4 @@
-package com.example.androidsimpleboilerplates.presentation.start
+package com.example.androidsimpleboilerplates.presentation.user
 
 import android.os.Bundle
 import android.view.LayoutInflater
