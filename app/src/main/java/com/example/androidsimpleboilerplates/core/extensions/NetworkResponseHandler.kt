f5777557f2c5bcba5f701386924d52e62fcc1681
imtiaz <programmer.imtiaz@gmail.com>
2022-10-22 13:09:51 +0600

    refactor: SafeApiCall method updated for managing error response

--- app/src/main/java/com/example/androidsimpleboilerplates/core/extensions/NetworkResponseHandler.kt
+++ app/src/main/java/com/example/androidsimpleboilerplates/core/extensions/NetworkResponseHandler.kt
@@ -1,52 +1,98 @@
 package com.example.androidsimpleboilerplates.core.extensions
 
+import com.google.gson.Gson
 import com.google.gson.stream.MalformedJsonException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import org.json.JSONException
 import retrofit2.HttpException
 import retrofit2.Response
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.net.SocketTimeoutException
 
-suspend inline fun <reified T> safeApiCall(
-    crossinline processApiCall: suspend () -> Response<T>
-): Flow<Resource<T>> = flow {
+/**
+ * Safe api call helps to process a network call
+ * and return a flow and emit corresponding data
+ * from that Api call with State by depending on Success, Loading, Error State
+ *
+ * @param T -> a generic State base data that going to be contain successful Api response or error data
+ * @param processApiCall -> contain a higher order suspended function
+ * @receiver
+ * @return -> return a flow and emit corresponding data from Api call with State
+ */
+suspend inline fun <reified REQUEST> safeApiCall(
+    crossinline processApiCall: suspend () -> Response<REQUEST>
+): Flow<Resource<REQUEST>> = flow {
     try {
+        // emitting loading state
         emit(Resource.Loading())
+
+        // calling api and processing the api response
         val response = processApiCall.invoke()
         val data = handleApiResponse(response)
 
-        if (data is T) emit(Resource.Success(data))
-        else emit(Resource.Error(Throwable(data as Exception)))
+        // if the data matches with the data we're expecting returning success state with that response
+        // otherwise returning error state with error data
+        if (data is REQUEST) emit(Resource.Success(data))
+        else emit(Resource.Error(data as ErrorHandler))
     } catch (e: Exception) {
-        emit(Resource.Error(Throwable(Exception(getCustomErrorMessage(e)))))
+        emit(Resource.Error(getCustomErrorMessage(e)))
     }
 }.flowOn(Dispatchers.IO)
 
-fun <T> handleApiResponse(response: Response<T>): Any? {
+/**
+ * Handle api response helps to parse the response as expected result and return.
+ * If failed to parse then returns error.
+ *
+ * @param REQUEST -> expected data class
+ * @param response -> contains api response
+ * @return Any -> If a successful parse return expected result, if error return error result
+ */
+fun <REQUEST> handleApiResponse(response: Response<REQUEST>): Any? {
     return try {
+        // Parse body
         if (response.isSuccessful) response.body()
         else {
-            // TODO Need to get error from here
+            // if anyhow got failed to parse Response, then trying to parse error response
             val json = response.errorBody()?.string()
-            Exception(json)
+            Gson().fromJson(json, ErrorHandler::class.java)
         }
     } catch (exp: Exception) {
+        // In the meantime of parsing response any exception happened we're returning error
         exp.printStackTrace()
-        exp
+        ErrorHandler("Something Went Wrong!", JSONException(exp.message), response.code())
     }
 }
 
-fun getCustomErrorMessage(error: Exception): String {
+/**
+ * Any exception happen to call a api or emitting data then return custom error message
+ *
+ * @param error -> contains throwable exception
+ * @return [ErrorHandler] with custom error message and exception
+ */
+fun getCustomErrorMessage(error: Throwable): ErrorHandler {
     return when (error) {
-        is SocketTimeoutException -> "Oh! We couldn't capture your request in time. Please try again."
-        is MalformedJsonException -> "Oh! We hit an error. Try again later."
-        is IOException -> "Oh! You are not connected to a wifi or cellular data network. Please connect and try again"
-        is FileNotFoundException -> "Oh! No such file or directory"
-        is HttpException -> error.message()
-        else -> "Something Went Wrong!"
+        is SocketTimeoutException -> ErrorHandler(
+            "Oh! We couldn't capture your request in time. Please try again.",
+            error
+        )
+        is android.util.MalformedJsonException -> ErrorHandler(
+            "Oh! We hit an error. Try again later.",
+            error
+        )
+        is FileNotFoundException -> ErrorHandler(
+            "Oh! No such file or directory",
+            error
+        )
+        is IOException -> ErrorHandler(
+            "Oh! You are not connected to a wifi or cellular data network. Please connect and try again",
+            error
+        )
+        is HttpException -> ErrorHandler(error.message(), error)
+
+        else -> ErrorHandler("Something Went Wrong!!", error as Exception)
     }
 }
